abdfda8f5db0f221c8bac5f4c1a9c877ccbad516
ccjjyy <153336897@qq.com>
2017-06-14 22:24:09 +0800

    Create pointer.c

--- pointer.c
+++ pointer.c
@@ -1,8 +1,8 @@
 // This is my tutorial of pointer
 
-//--------------------------------------------
-// Notice how to use the name of a multi-array
-//--------------------------------------------
+//--------------------------------------
+// How to use the name of a multi-array?
+//--------------------------------------
 
 #include "stdio.h" 
 
@@ -19,10 +19,52 @@ int main(){
 }
 
 
+//----------------------------------------------------
+// How to use the pointers as the function parameters?
+//----------------------------------------------------
 
+#include "stdio.h" 
 
+void printArray1(int (*pointer)[4]);
+void printArray2(int (*pointer)[4], int n);
 
+int main(){
+	int array[3][4] = {{1,2,3,4},
+			   {5,6,7,8},
+			   {9,10,11,12}};
+	int (*pointer)[4], count;
+	pointer = array;
+	
+	for(count=0; count<3; count++){
+		printArray1(pointer++);
+	}
+	
+	puts("\n\nPress Enter...");
+	getchar();
+	
+	printArray2(array, 3);
+	
+	return 0;
+}
 
+// Functions declarations
+void printArray1(int (*pointer)[4]){     // Notice int *pointer[4]
+	int *p, count;
+	p = (int *)pointer;              // Casting
+	
+	for(count=0; count<4; count++){
+		printf("\n%d", *p++);
+	}
+}
+
+void printArray2(int (*pointer)[4], int n){
+	int *p, count;
+	p = (int *)pointer;
+	
+	for(count=0; count<(4*n); count++){
+		printf("\n%d", *p++);
+	}
+}
 
 
 
