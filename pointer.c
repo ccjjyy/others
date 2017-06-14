f45ebe5dec795c3adc5792b3e954b540cf4b83d9
ccjjyy <153336897@qq.com>
2017-06-14 21:46:27 +0800

    Create pointer.c

--- pointer.c
+++ pointer.c
@@ -9,11 +9,11 @@
 int main(){
 	int array[2][4] = {1,2,3,4,5,6,7,8};
 	
-	printf("%d\n", **array);
-	printf("%d\n", **(array+1));
+	printf("%d\n", **array);        // array[0][0] = 1
+	printf("%d\n", **(array+1));    // array[1][0] = 5
 	
-	printf("%d\n", *array[1]);
-	printf("%d\n", *(array[1]+1));
+	printf("%d\n", *array[1]);      // array[1][0] = 5
+	printf("%d\n", *(array[1]+1));  // array[1][1] = 6
 	
 	return 0;
 }
