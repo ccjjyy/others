221c16d694afd497321d08a7336853dac7c09e23
ccjjyy <153336897@qq.com>
2017-06-22 17:36:54 +0800

    Create basis.java

---
+++ basis.java
@@ -0,0 +1,13 @@
+package org.cjy.learning;
+
+// Notice the differnce between public class & class.
+public class Main { 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 10;
+		num = 30;
+		System.out.println("num is: " + num);
+		System.out.println("num * num is: " + num * num);
+	}
+}
